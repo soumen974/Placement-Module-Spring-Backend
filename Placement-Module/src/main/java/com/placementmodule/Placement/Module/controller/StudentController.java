9ccb9f99dce9a7feeffcbc77b46f33804443bbbd
soumen bhunia <sob99338@gmail.com>
2023-11-14 00:26:03 +0530

    controller connected and post and get map works through postman

--- Placement-Module/src/main/java/com/placementmodule/Placement/Module/controller/StudentController.java
+++ Placement-Module/src/main/java/com/placementmodule/Placement/Module/controller/StudentController.java
@@ -1,2 +1,25 @@
-package com.placementmodule.Placement.Module.controller;public class StudentController {
+package com.placementmodule.Placement.Module.controller;
+
+import com.placementmodule.Placement.Module.model.Student;
+import com.placementmodule.Placement.Module.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class StudentController {
+   @Autowired
+    private StudentRepository studentRepository;
+
+   @PostMapping("/student")
+    Student newStudent(@RequestBody Student newStudent){
+       return studentRepository.save(newStudent);
+   }
+   @GetMapping("/student")
+    List<Student> getAllStudents(){
+       return studentRepository.findAll();
+   }
+
 }
