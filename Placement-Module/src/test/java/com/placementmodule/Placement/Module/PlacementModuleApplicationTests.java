c52f38bf120108dd59fc8e2192f440ff06415b22
soumen bhunia <sob99338@gmail.com>
2023-11-13 23:20:21 +0530

    backend working on

---
+++ Placement-Module/src/test/java/com/placementmodule/Placement/Module/PlacementModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placementmodule.Placement.Module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
