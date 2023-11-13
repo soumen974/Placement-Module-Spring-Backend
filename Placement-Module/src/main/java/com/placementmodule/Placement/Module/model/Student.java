ba802a883eb60ea3d69ae183cc40905b84453edd
soumen bhunia <sob99338@gmail.com>
2023-11-14 00:25:11 +0530

    student entity added

--- Placement-Module/src/main/java/com/placementmodule/Placement/Module/model/Student.java
+++ Placement-Module/src/main/java/com/placementmodule/Placement/Module/model/Student.java
@@ -1,2 +1,128 @@
-package com.placementmodule.Placement.Module.model;public class Student {
+package com.placementmodule.Placement.Module.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue
+
+    private Long id;
+    private Long regdno;
+
+    private String usertype;
+    private String name;
+    private String email;
+
+    private Long mob;
+
+    private String profilePicturePath;
+
+    private String program;
+    private String branch;
+
+    private Long year;
+
+    private String campus;
+
+    private Long appliedjobbyid;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRegdno() {
+        return regdno;
+    }
+
+    public void setRegdno(Long regdno) {
+        this.regdno = regdno;
+    }
+
+    public String getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getMob() {
+        return mob;
+    }
+
+    public void setMob(Long mob) {
+        this.mob = mob;
+    }
+
+    public String getProfilePicturePath() {
+        return profilePicturePath;
+    }
+
+    public void setProfilePicturePath(String profilePicturePath) {
+        this.profilePicturePath = profilePicturePath;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public Long getYear() {
+        return year;
+    }
+
+    public void setYear(Long year) {
+        this.year = year;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+    public Long getAppliedjobbyid() {
+        return appliedjobbyid;
+    }
+
+    public void setAppliedjobbyid(Long appliedjobbyid) {
+        this.appliedjobbyid = appliedjobbyid;
+    }
 }
